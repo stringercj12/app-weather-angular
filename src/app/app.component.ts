8e38adb67955a1d1fb69e230caa5dc6d804653ca
Jefferson Ferreira <jefferson14489@gmail.com>
2021-01-29 11:14:23 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment-timezone';
+import 'moment/locale/pt-br';
+
 @Component({
-  selector: 'app-root',
+  selector: 'jv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-weather-angular';
+
+  constructor() {
+    moment.locale('pt-br');
+  }
 }
